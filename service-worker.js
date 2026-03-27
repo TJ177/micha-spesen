4ea5edf5bdf04a6a4df558d6ff1d9c02e1befb9a
TJ177 <weotp177@gmail.com>
2026-03-27 10:39:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "micha-spesen-v1";
+const CACHE_NAME = "micha-spesen-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
